cbcee8c5405937e4ef55ee44bd51d37efb382989
unknown <augustomedeiros87@gmail.com>
2021-06-01 20:53:56 -0300

    Projeto Cmail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cmail';
+  private _isNewEmailFormOpen = false;
+
+  get isNewEmailFormOpen() {
+    return this._isNewEmailFormOpen;
+  }
+  
+  toggleNewEmailForm() {
+    this._isNewEmailFormOpen = !this.isNewEmailFormOpen
+  }
+
 }
