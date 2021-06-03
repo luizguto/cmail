118374fb383911ba7d1d418408dd75c79df0e5bf
unknown <augustomedeiros87@gmail.com>
2021-06-03 19:32:00 -0300

    ex15

---
+++ src/app/modules/caixa-de-entrada/caixa-de-entrada.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CaixaDeEntradaComponent } from './caixa-de-entrada/caixa-de-entrada.component';
+
+
+
+@NgModule({
+  declarations: [
+    CaixaDeEntradaComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CaixaDeEntradaModule { }
