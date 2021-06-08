ed7ee3ee7e9e97c0c03f70ed85760a13c865bac3
unknown <augustomedeiros87@gmail.com>
2021-06-07 21:46:49 -0300

    ex29

--- src/app/modules/cadastro/cadastro.component.ts
+++ src/app/modules/cadastro/cadastro.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
-import { HttpClient, HttpResponseBase } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpResponseBase } from '@angular/common/http';
 import { map, catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-cadastro',
@@ -9,6 +11,8 @@ import { map, catchError } from 'rxjs/operators';
   styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent implements OnInit {
+
+  mensagensErro: any;
 
   formCadastro = new FormGroup({
     nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
@@ -18,20 +22,12 @@ export class CadastroComponent implements OnInit {
     avatar: new FormControl('', [Validators.required], this.validaImagem.bind(this)),
   });
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient
+    , private roteador: Router) { }
 
   ngOnInit(): void {
   }
 
-  handleCadastrarUsuario() {
-    if (this.formCadastro.valid) {
-      console.log(this.formCadastro.value);
-      this.formCadastro.reset();
-    }
-    else {
-      this.validarTodosOsCamposDoFormulario(this.formCadastro);
-    }
-  }
 
   validarTodosOsCamposDoFormulario(form: FormGroup) {
     Object.keys(form.controls).forEach(field => {
@@ -55,5 +51,28 @@ export class CadastroComponent implements OnInit {
       )
   }
 
+  handleCadastrarUsuario() {
+    if (this.formCadastro.valid) {
+      const userData = new User(this.formCadastro.value);
+      this.httpClient
+        .post('http://localhost:3200/users', userData)
+        .subscribe(
+          (response) => {
+            console.log(`Cadastrado	com	sucesso`);
+            this.formCadastro.reset()
+            setTimeout(() => {
+              this.roteador.navigate(['']);
+            }, 1000);
+          }
+          , (responseError: HttpErrorResponse) => {
+            //resposta	caso	existam	erros!
+            this.mensagensErro = responseError.error.body
+          }
+        )
+    }
+    else {
+      this.validarTodosOsCamposDoFormulario(this.formCadastro);
+    }
+  }
 
 }
